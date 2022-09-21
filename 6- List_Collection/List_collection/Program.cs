654dc8abab8e642a44ab1fd7c3d1a75b53cdac53
yoncademirhan <99687715+yoncademirhan@users.noreply.github.com>
2022-09-21 20:05:00 +0300

    Add files via upload

---
+++ 6- List_Collection/List_collection/Program.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List_collection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* List<int> sayilar = new List<int>();*/
+
+            //List <T> koleksiyonu içerisinde değer ekleme  işlmei. Add(<deger>); metodu ile yapabilirsiniz.
+
+            sayilar.Add(15);
+             sayilar.Add(48);
+             sayilar.Add(37);
+
+            //List <T> koleksiyonu içerisindeki eleman sayısını öğrenmek için  .Count proprty'sini kullanabilirsiniz.
+            //Bu property geriye int değer döndürr.
+
+            Console.WriteLine("Koleksiyon içerisindeki eleman sayısı: " + sayilar.Count);
+
+            //koleksiyon içewrisindeki degerleri okuyalım(for);
+             for(int i = 0; i < sayilar.Count; i++)
+             {
+                 Console.Write(sayilar[i] + " ");
+             }
+             Console.WriteLine();
+
+             //koleksiyon içewrisindeki degerleri okuyalım(foreach)
+
+             foreach(var sayi in sayilar)
+             {
+                 Console.Write(sayi + " ");
+             }
+
+             Console.WriteLine();
+
+
+            //List<T> koleksiyonu içerisindeki değerleri çıkartmak için 2 farklı metot kullanılır.Remove() ve RemoveAt() metotlarıdır.
+            //Remove (<deger>); remove metotu içeriine değer belirtilir. Belirtilen değeri koleksiyon içerinden cıakrtır.
+            //RemoveAt(<indexNumarasi>); RemoveAt metotu içeriine index numarası belirtilir. Belirtilen ndex numarasındaki eleman listeden cıkartır.
+
+             sayilar.Remove(48);
+             sayilar.RemoveAt(0);  //,index numarasına göre seçim yapılır.
+
+
+             foreach (var sayi in sayilar)
+             {
+                 Console.Write(sayi + " ");
+             }
+
+             Console.WriteLine();
+
+            //List<T> koleksiyonu içerisindeki tüm elemanları silmek için Clear() metotu kullanılır.
+            // sayilar.Clear();  // Koleksiyon içeriisndeki tüm elemanları siler.
+
+
+
+
+            //Örnek: Kullanıcıdan 10 adet şehir ismi talep edelim. Talep ettiğimiz şehir isimlerini koleksiyon içerisinde baarındıralım.
+            //Koleksiyon içeriindeki verileri ekrana yazdıralım.
+
+            //Çözüm:
+            ist<string>sehirler = new List<string>();
+
+             for (int i = 0; i < 10; i++)
+             {
+                 Console.Write($"{i + 1}.şehir bilgisini giriniz.");
+                 sehirler.Add(Console.ReadLine());
+             }
+
+             Console.WriteLine();
+             Console.WriteLine("Şehirler");
+             Console.WriteLine("--------------");
+             foreach (var sehir in sehirler)
+             { 
+                 Console.WriteLine(sehir);
+             }
+
+
+
+            //Örnek: Sayı tutma ve toplama programı
+            /* 1- sayı gir
+             * 2- sayıları listele
+             * 3- sayı sil
+             * 4-Sayıların Toplamı
+             * 5- Sayıların ortalamsı
+             * X programdan cık  */
+
+
+
+            ConsoleKey islem;
+            List<double> sayilar = new List<double>();
+
+            do
+            {
+                Console.Clear();
+
+                Console.WriteLine("Sayı tutma ve toplama programı");
+                Console.WriteLine("----------------------------------");
+                Console.WriteLine("1- sayı gir");
+                Console.WriteLine("2- sayıları listele");
+                Console.WriteLine("3- sayı sil");
+                Console.WriteLine("4-Sayıların Toplamı");
+                Console.WriteLine("X programdan cık ");
+                Console.WriteLine();
+                Console.Write("Yapmak istediğiniz işlemi seçiniz: ");
+                islem = Console.ReadKey().Key;
+
+                Console.Clear();
+
+                switch (islem)
+                {
+                    case ConsoleKey.D1:
+                    case ConsoleKey.NumPad1:
+                        //Sayı gir
+                        Console.WriteLine("Eklemek istediğiniz sayıyı giriniz: ");
+                        sayilar.Add(double.Parse(Console.ReadLine()));
+
+                        Console.WriteLine();
+                        Console.WriteLine("Sayı başarılı bir şekilde eklendi.");
+                        Console.WriteLine("Ana menüye dönmek için bir tuşa basınız.");
+                        Console.ReadKey();
+                        break;
+
+                    case ConsoleKey.D2:
+                    case ConsoleKey.NumPad2:
+                        //komut satırı
+                        Console.WriteLine("girilen sayılar");
+                        Console.WriteLine("-------------");
+                        foreach (var sayi in sayilar)
+                        {
+                            Console.Write(sayi + " ");
+                        }
+                        Console.WriteLine();
+                        Console.WriteLine($"{sayilar.Count} adet sayı listelendi");
+                        Console.ReadKey();
+                        break;
+
+                    case ConsoleKey.D3:
+                    case ConsoleKey.NumPad3:
+                        //komut satırı
+                        Console.WriteLine("girilen sayılar");
+                        Console.WriteLine("-------------");
+                        for(int i=0; i < sayilar.Count; i++)
+                        {
+                            Console.WriteLine($"{i + 1}. sayı: {sayilar[i]}");
+                        }
+                        Console.WriteLine();
+                        Console.Write("Silmek istediğiniz sayının sıra numarasını giriniz: ");
+                        int siraNo = int.Parse(Console.ReadLine());
+
+                        sayilar.RemoveAt(siraNo - 1);
+
+                        Console.WriteLine("Seçmiş oldugunuz sayı silmiştir.");
+                        Console.WriteLine("Ana menüye dönmek için bir tuşa basınız.");
+                        Console.ReadKey();
+                        break;
+
+                    case ConsoleKey.D4:
+                    case ConsoleKey.NumPad4:
+                        //komut satırı
+                        double toplam = 0;
+                        foreach( var sayi in sayilar)
+                        {
+                            toplam += sayi;
+                        }
+                        Console.WriteLine("girilen sayıların toplamı: " + toplam);
+                        Console.WriteLine();
+                        Console.WriteLine("Ana menüye dönmek için bir tuşa basınız.");
+                        Console.ReadKey();
+                        break;
+
+                    case ConsoleKey.D5:
+                    case ConsoleKey.NumPad5:
+                        //komut satırı
+                        double toplam2 = 0;
+                        foreach ( var sayi in sayilar)
+                        {
+                            toplam2 += sayi;
+                        }
+                        Console.WriteLine("girilen sayıların ortalaması: " + (toplam2 / sayilar.Count));
+                        Console.WriteLine();
+                        Console.WriteLine("Ana menüye dönmek için bir tuşa basınız.");
+                        Console.ReadKey();
+                        break;
+                }
+
+
+            } while (islem != ConsoleKey.X);
+
+            Console.WriteLine();
+            Console.WriteLine("Programı kullandığınız için teşekkür ederiz");
+            Console.WriteLine("Uygulamayı kapğatmak için bir tuşa basınız");
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 6- List_Collection/List_collection/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("List_collection")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("List_collection")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("4f6a123a-bb00-4a69-a3e8-efb9621b5e3a")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
