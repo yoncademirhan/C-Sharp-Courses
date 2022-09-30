40e21367db5bc990be32288ac505cfaa83694975
yoncademirhan <99687715+yoncademirhan@users.noreply.github.com>
2022-09-30 12:45:33 +0300

    Add files via upload

---
+++ 11- Class_Yapisi/Class_Yapisi/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Yapisi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Class Yapısı
+    * -------------------
+    * Class bir nesnenin şeklini tanımlayan şablondur. Sınıf, verileri ve bu verileri işleyecek kodu içeriri.
+    * Sınıf nesneleri kendi içerisinde temel özellikleri içerir.
+    * Nesneler sınıfların birer örneğidirler.
+    * Bu durumda sınıf, nesnelerin nasıl inşa edileceğini tanımlayan bir kılavuzdur.
+    * Gerçek hayatta birçok nesne bulunur. Bunlardan bazıları Kitap, kalem, televizyon, araba, vs...
+    * Programlama içerieindede bu durum farksızdır. Yani hayatımını modelleyerej programlama içerisine aktarabiliriz.
+    * Bu nesnelerin belirli özellikleri ve fonksiyonları vardır.
+    * Mesela arabanın kapısının olması veya hareket etmesi gibi...
+    * Arabanın kapısı özelliği, arabanın gitmesi işleyişi(fonksiyon) ifade eder.
+    *
+    * Class'kar C# programlama dilinde namespace içerisine yazılmaktadır.
+    *
+    * Classlar içerisinde birçok özellik barındırılabilir.
+    * Bunlardan bazılar:
+    * 1- Field
+    * 2- Property
+    * 3- Metod
+    * 4- Constructer Metod
+    * vs....
+    *
+    * Sınıf soyut bir ifadedir, nesneler oluşuncaya kadar fiziksel olarak bellekte yer almazlar. Sınıfta veriler veri üyelerinde(field), kod ise fonksiyon üyelerinde saklanır. Veri üyeleri örnek değişkenleri ve statik değişkenleri içerir. Fonksisiyon üyeleri ise metodları, yapıcı metodlar, vs.. içerirler.
+    *
+    * Class tanımla syntax'i
+    * --------------------------
+    * <erisimTuru> class <classAdi>{
+    *      Class içerisindeki özellikler
+    * }
+    *
+    */
+        }
+    }
+}
