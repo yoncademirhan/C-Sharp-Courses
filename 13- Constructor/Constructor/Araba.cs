c0e15165a9a6d0df9dcd3c21bc6b743bef418cb7
yoncademirhan <99687715+yoncademirhan@users.noreply.github.com>
2022-10-11 08:29:36 +0300

    Add files via upload

---
+++ 13- Constructor/Constructor/Araba.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    public class Araba
+    {
+
+       // public string Marka;
+        public string Marka{ get; set; }
+
+        public string Model { get; set; }
+        public string Renk { get; set; }
+
+
+        public void Yazdir()
+        {
+            Console.WriteLine("Marka: " + Marka);
+            Console.WriteLine("Model: " + Model);
+            Console.WriteLine("Renk: " + Renk);
+        }
+
+
+
+
+
+
+
+        public Araba()
+        {
+            Console.WriteLine("Yeni bir Araba Class oluşturuldu");
+        }
+
+
+        public Araba(string _marka, string _model, string _renk)
+        {
+            Marka = _marka;
+            Model = _model;
+            Renk = _renk;
+        }
+    }
+}
---
+++ 13- Constructor/Constructor/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Constructor: Nesne yönelimli programlama kavramı içerisinde bulunan sınıf yapılarını nesne oalrk tanımlanmasında alt yapıyı
+            hazırlayan, kurucu rolü üstlenen, sınıf ile aynı isme sahip olan geriye deger döndürmeyen fonksiyon türüdür.
+
+            * Yalnızca sınıf cagırma esnasında new ifadesi ile birlikte çalışır.sonradan cagırma imkanınız yoktur.
+            
+            * Constructor metod ismi class ismi ile aynı olmak zorundadır.
+            
+            * Geriye deger döndürmedikleri için void yapıda tanımlanmalıdır.
+            
+            * Erişim belirleyici mutlaka public olmalıdır.Aksi takdirde Constructor metot olarak işlevi gerçekleştiremez.
+             
+             */
+
+
+            Araba a1= new Araba();
+            a1.Marka = "BMW";
+            a1.Model = "X5";
+            a1.Renk = "Siyah";
+
+            a1.Yazdir();
+
+            Console.WriteLine();
+
+            Araba a2 = new Araba("Audi", "A6", "Beyaz");
+            a2.Yazdir();
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
