469eff4485092f63e9633dfabea4ae47a4494580
yoncademirhan <99687715+yoncademirhan@users.noreply.github.com>
2022-10-12 13:13:17 +0300

    Add files via upload

---
+++ 14- Enum/Enum/Gun.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum
+{
+    internal enum Gun
+    {
+        Pazartesi =1,
+        Sali,
+        Carsamba,
+        Persembe,
+        Cuma,
+        Cumartesi,
+        Pazar
+    }
+}
---
+++ 14- Enum/Enum/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum
+{
+
+    /*Enum(numaralndırmalar): 
+
+           Numaralndırmalar(enumerations):
+           kod içerisinde sayısal karşılaştırma veya işlem gerektiren yerlerde yazılımcı için daha okunabilirlik sunan kod karmaşasını azaltan yardımcı bir yapıdır.
+
+           Syntax----
+           <erisimtipi> <enumAdi> {<deger1> <deger2>, <deger3>, ...}
+
+           *Enum yapısını kısa yoldan oluşturmak için yeni bir class oluşturup class degerini enum ile değştirebilirsiniz
+           *
+           *Enum içerisine deger vermezsek, degerlersıfır dan başlar.
+           *Enumların varsayılan degeri int.dır.
+           *
+
+            */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int bugün =(int) Gun.Cumartesi;
+
+           if(bugün == (int)Gun.Cumartesi || bugün == (int)Gun.Pazar)
+            {
+                Console.WriteLine("Hafta sonu");
+            }
+            else
+            {
+                Console.WriteLine("Hafta içi");
+            }
+
+            
+
+           Console.ReadKey();
+
+        }
+
+    }
+}
