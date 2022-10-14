2837c67bce03f4729bdaccdbe0171c13765b9214
yoncademirhan <99687715+yoncademirhan@users.noreply.github.com>
2022-10-14 07:56:39 +0300

    Add files via upload

---
+++ 16- Abstraction/20220825_4_Abstraction/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20220825_4_Abstraction
+{
+    // Abtraction(Soyutlama)
+    // Karmaşıklığı yönetmek için kullanılır. Nesnenin diğer tüm nesne türlerinden ayıran temel özelliklerini belirtir, böylece izleyicinin bakış açısından açıkça tanımlanmış bir kavramsal sınır sağlar.
+    // Nesne yönelimli programlamalarda, ayırma, ayrıntıları tanımlamaktan ziyade sınıflar ve yöntemler için temel görevleri tanımlamak için kullanılır. 
+    // Temel olarak,problemi çözmek için kullanılan öncelikle daha genel, daha basit ve soyuttur.
+
+    // Abstract classlar soyut sınıflardır.
+
+    public abstract class Sekil
+    {
+        public int Cevre()
+        {
+            return 0;
+        }
+    }
+
+
+
+    public class Dikdortgen : Sekil
+    {
+        public int En { get; set; }
+        public int Boy { get; set; }
+
+        public int Cevre()
+        {
+            return (2 * (En + Boy));
+        }
+    }
+}
---
+++ 16- Abstraction/20220825_4_Abstraction/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20220825_4_Abstraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dikdortgen d = new Dikdortgen();
+            d.Boy = 5;
+            d.En = 3;
+            Console.WriteLine("Dikdörtgenin Çevresi: " + d.Cevre());
+
+            Console.ReadKey();
+        }
+    }
+}
